976b4e80ccb8eb291308db08e4a87d2964370e93
jerry44000 <shai.badarna@gmail.com>
2022-01-25 20:00:36 +0100

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = ({movie}) => {
+    const dateFormater = (date) => {
+        let [yy, mm, dd] = date.split("-");
+        return [dd, mm, yy].join("/")
+    }
+    return (
+        <div className="card">
+            <img src={movie.poster_path ? "https://image.tmdb.org/t/p/w500" + movie.poster_path : "./img/poster.jpg"} alt="affiche de film" />
+            <h2>{movie.title}</h2>
+            {movie.release_date ? (
+                <h5>Sortie le : {dateFormater(movie.release_date)}</h5>
+            ) : ("")
+        }
+        <h4>{movie.vote_average}/10 <span>⭐</span></h4>
+        </div>
+    );
+};
+
+export default Card;
